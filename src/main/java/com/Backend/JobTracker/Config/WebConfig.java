5227d6588f875de823dbd431c5011ee020f382f2
PrasannaK-27 <pk6500642@gmail.com>
2026-06-12 20:49:29 +0530

    Config commit...Updated forntendUrl in spring!!

--- src/main/java/com/Backend/JobTracker/Config/WebConfig.java
+++ src/main/java/com/Backend/JobTracker/Config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins(
                         "http://localhost:5173",
                         "http://localhost:3000",
-                        "https://YOUR-REACT-APP.catalystappsail.in"
+                        "intenshiptracking.netlify.app"
                 )
                 .allowedMethods("GET", "POST", "PUT", "PATCH" , "DELETE", "OPTIONS")
                 .allowedHeaders("*")
